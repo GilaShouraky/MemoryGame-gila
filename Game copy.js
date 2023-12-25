d57e276dc1f8eb08ed42075d0a86a46b4dd5f4a6
Gila Shouraky <gilasho1234@gmail.com>
2023-12-25 16:26:55 +0200

    Final game

--- Game copy.js
+++ Game copy.js
@@ -1,16 +1,17 @@
-const mainArray = ["url(חנוכיה.png)", "url(כד.png)", "url(נר.png)", "url(סביבון.png)", "url(סופגניה.png)"];
-let arrayAfterShuffling = shuffle(mainArray.concat(mainArray));
-let board = document.getElementsByClassName("board")[0]
+const mainArray = ["url(חנוכיה.png)", "url(כד.png)", "url(נר.png)", "url(סביבון.png)", "url(סופגניה.png)", "url(סופגניה2.png)", "url(חנוכיה4.png)", "url(חנוכיה2.png)", "url(חנוכיה3.png)", "url(ילדים.png)", "url(כד2.png)", "url(מטבעות.png)", "url(סביבון2.png)", "url(שיר.png)", "url(סופגניה3.png)"];
 function createCard(num) {
-  for (i = 0; i <= num; i++) {
-    let card = document.createElement("div") //מוסיף div
-    card.classList.add("card") // מכניס class="card" לתוך הדיב
-    card.value = arrayAfterShuffling[i]
-    board.appendChild(card)
-    card.onclick = frontCard
+  let arrayAfterShuffling = shuffle((mainArray.slice(0, (num / 2))).concat(shuffle(mainArray.slice(0, (num / 2)))));
+  console.log(arrayAfterShuffling);
+  const board = document.getElementsByClassName("board")[0];
+  board.innerHTML = ''; // מוחק את כל התוכן שבתוך הלוח
+  for (let i = 0; i < num; i++) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.value = arrayAfterShuffling[i];
+    board.appendChild(card);
+    card.onclick = frontCard;
   }
 }
-createCard(9)
 
 function shuffle(shuffleArr) {
   for (i = 0; i < shuffleArr.length * 2; i++) {
@@ -36,22 +37,75 @@ function frontCard(e) {
   openingTwoCards(tor, e)
 }
 
+function handleLevelChange(value) {
+  if (value === "30") {
+    const board = document.getElementsByClassName("board")[0];
+    board.classList.toggle("board30");
+    board.classList.remove("board20")
+    board.classList.remove("board10")
+    document.getElementById("myElement").style.display = "none"
+    document.getElementById("Element").style.display = "none"
+    createCard(30);
+    document.getElementById("audio2").play()
+    clearInterval(interval);
+    countdown(60);
+  }
+  else if (value === "20") {
+    const board = document.getElementsByClassName("board")[0];
+    board.classList.toggle("board20");
+    board.classList.remove("board30")
+    board.classList.remove("board10")
+    document.getElementById("myElement").style.display = "none"
+    document.getElementById("Element").style.display = "none"
+    createCard(20);
+    document.getElementById("audio2").play()
+    clearInterval(interval);
+    countdown(60);
+  }
+  else if (value === "10") {
+    const board = document.getElementsByClassName("board")[0];
+    board.classList.toggle("board10");
+    board.classList.remove("board20")
+    board.classList.remove("board30")
+    document.getElementById("myElement").style.display = "none"
+    document.getElementById("Element").style.display = "none"
+    createCard(10);
+    document.getElementById("audio2").play()
+    clearInterval(interval);
+    countdown(60);
+  }
+  else {
+    createCard(0);
+  }
+}
 
 let tor = []
+
 function openingTwoCards(arrElement, e) {
   arrElement.push(e.target.style.background);
-  console.log(arrElement);
-  if (arrElement.length == 2) {
-    if (arrElement.slice(-2)[0] === arrElement.slice(-2)[1]) {
-      ifCouple(arrElement)
-    }
-    else {
-      ifNOTCouple(arrElement)
+
+  if (arrElement.length === 2) {
+    if (arrElement[0] === arrElement[1]) {
+      ifCouple(arrElement);
+    } else {
+      ifNOTCouple(arrElement);
     }
     resetArray(arrElement);
     console.log(arrElement);
   }
-  checkWin()
+  if (document.getElementsByClassName('frontCard').length == 2) {
+    const cards = document.getElementsByClassName('card');
+    Array.from(cards).forEach(card => {
+      card.style.pointerEvents = "none";
+    });
+  }
+  setTimeout(function () {
+    const cards = document.getElementsByClassName('card');
+    Array.from(cards).forEach(card => {
+      card.style.pointerEvents = "";
+    });
+  }, 1700)
+  checkWin();
 }
 
 function resetArray(arr) {
@@ -65,6 +119,7 @@ function ifCouple(arrElement) {
     cardElement.classList.add("finding");
     cardElement.classList.remove("frontCard");
   })
+  document.getElementById("audio").play()
   console.log(arrElement[0], arrElement[1],);
 }
 
@@ -77,12 +132,14 @@ function ifNOTCouple(arrElement) {
       console.log("😒");
       console.log(arrElement);
     })
-  }, 1500)
+  }, 1000)
 }
 
 function checkWin() {
-  if (document.getElementsByClassName('finding').length == 10) {
+  if (document.getElementsByClassName('finding').length == document.getElementsByClassName('card').length) {
     document.getElementById("myElement").style.display = "block";
+    document.getElementById("Element").style.display = "none"
+    document.getElementById("audio3").play()
   }
 }
 
@@ -95,21 +152,27 @@ function restart() {
   })
   document.getElementById("myElement").style.display = "none"
   document.getElementById("Element").style.display = "none"
-  countdown(1)
+  clearInterval(interval);
+  countdown(60)
 }
-
-function countdown(minutes) {
-  let seconds = minutes * 60;
-  const endTime = new Date().getTime() + seconds * 1000;
-  const interval = setInterval(() => {
-    const remainingTime = Math.round((endTime - new Date().getTime()) / 1000);
-    let time = document.getElementById("time")
-    time.innerText = `הזמן שנותר: ${remainingTime} שניות`;
-    if (remainingTime <= 0) {
-      clearInterval(interval);
+let interval;
+function countdown(num) {
+  let timerElement = document.getElementById("time")
+  let timerCount = num
+  interval = setInterval(() => {
+    --timerCount;
+    if (timerCount >= 10 && timerCount < 60) {
+      timerElement.innerText = '0:' + timerCount
+    }
+    else if (timerCount > 0) {
+      timerElement.innerText = '0:0' + timerCount
+    }
+    else {
+      timerElement.innerText = '0:00'
+      clearInterval(interval)
       document.getElementById("Element").style.display = "block";
+      document.getElementById("audio2").pause()
+      document.getElementById("audio4").play()
     }
   }, 1000);
 }
-
-countdown(1);
